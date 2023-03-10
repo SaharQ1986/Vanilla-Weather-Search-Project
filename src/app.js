1326ad317f2bff762c7916a40cd6534c398fc163
Sahar Qasemi <sahar.gh9244@gmail.com>
2023-03-10 19:16:20 +0330

    add codes for time

--- src/app.js
+++ src/app.js
@@ -1,15 +1,41 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minuts = date.getMinutes();
+  if (minuts < 10) {
+    minuts = `0${minuts}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}  ${hours}:${minuts}`;
+}
+
 function displayTemprature(respone) {
   console.log(respone);
   let tempratureElement = document.querySelector("#temprature");
-  tempratureElement.innerHTML = Math.round(respone.data.main.temp);
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = respone.data.name;
   let description = document.querySelector("#weather-description");
-  description.innerHTML = respone.data.weather[0].description;
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = respone.data.main.humidity;
   let windElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#city-date");
+
+  tempratureElement.innerHTML = Math.round(respone.data.main.temp);
+  cityElement.innerHTML = respone.data.name;
+  description.innerHTML = respone.data.weather[0].description;
+  humidityElement.innerHTML = respone.data.main.humidity;
   windElement.innerHTML = Math.round(respone.data.wind.speed);
+  dateElement.innerHTML = formatDate(respone.data.dt * 1000);
 }
 
 let apiKey = `a1e3ad83f9c55b1988611b4f77512fe8`;
